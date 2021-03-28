5fa8e96a39341b4821d4ae51d1b88578109cfbc0
marcoescorza117 <marcoescorza117@users.noreply.github.com>
2021-03-28 21:12:49 +0000

    deploy: f2f096a3a12d305277c46a155ab63d3a4a8a7aab

--- _dist_/index.js
+++ _dist_/index.js
@@ -4,3 +4,27 @@
  **/
 
 console.log('Happy hacking :)')
+
+const maximum = 122;
+const minimum = 1; 
+const random = ()=>  Math.floor(Math.random() * (maximum-minimum)+ minimum);
+    
+
+
+const createImageNode = ()=>{
+    const container = document.createElement('div');
+    container.className = "p-4";
+
+    const imagen = document.createElement('img');
+    imagen.className = "mx-auto";
+    imagen.width = "320";
+    imagen.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+    container.appendChild(imagen);
+    return container;  
+}
+
+
+const nuevaImagen = createImageNode();
+const mountNode = document.getElementById("images");
+mountNode.append(nuevaImagen);
